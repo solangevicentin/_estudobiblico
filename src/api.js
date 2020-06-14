f88b30e36567122ed63ba7a054d1c3edf86b15a2
solangevicentin <solange-vicentin@outlook.com>
2020-06-14 20:54:23 +0000

    Corrigindo problema de cors

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
  
 const api = axios.create({
-    baseURL: 'https://estudobiblico.herokuapp.com/livro'
+    baseURL: 'https://estudobiblico.herokuapp.com'
 });
  
 export default api;
